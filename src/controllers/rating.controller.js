2d45adbcdfe38f281820ecf1969e75a50643cbb4
Abdul Muheeth <abdul.muheeth@megaops.io>
2024-02-20 00:40:26 +0530

    ratings crud

--- src/controllers/rating.controller.js
+++ src/controllers/rating.controller.js
@@ -37,4 +37,29 @@ const getAverageRating = asyncHandler(async (req, res) => {
 	return res.status(201).json(new ApiResponse(200, avgRating, "Avg Rating Fetched"));
 });
 
-export { addMovieRating, getAllRatings, getAverageRating };
+const deleteRating = asyncHandler(async (req, res) => {
+	const userId = req.user._id;
+	const ratingId = req.params.ratingId;
+	const existingRating = await Rating.findById(ratingId);
+	const ratingOf = existingRating.userId;
+	if (userId.toString() !== ratingOf.toString()) {
+		throw new ApiError(400, "Unauthorized Access");
+	}
+	await Rating.findByIdAndDelete(ratingId);
+	res.status(200).json(new ApiResponse(200, {}, "Rating Deleted"));
+});
+
+const editRating = asyncHandler(async (req, res) => {
+	const ratingId = req.params.ratingId;
+	const userId = req.user._id;
+	const existingRating = await Rating.findById(ratingId);
+	if (userId.toString() !== existingRating.userId.toString()) {
+		throw new ApiError(400, "Unauthorized Access");
+	}
+	const movieId = req.params.movieId;
+	const rating = req.params.rating;
+	const uniqueId = movieId + userId;
+	const updatedRating = await Rating.findByIdAndUpdate(ratingId, { rating: rating, uniqueId: uniqueId }, { new: true });
+	return res.status(200).json(new ApiResponse(200, updatedRating, "Rating Updated"));
+});
+export { addMovieRating, getAllRatings, getAverageRating, deleteRating, editRating };
--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { addMovieRating, getAllRatings, getAverageRating } from "../controllers/rating.controller.js";
+import {
+	addMovieRating,
+	deleteRating,
+	editRating,
+	getAllRatings,
+	getAverageRating,
+} from "../controllers/rating.controller.js";
 
 const ratingRouter = Router();
 
 ratingRouter.route("/get-movie-ratings/:movieId").get(getAllRatings);
 ratingRouter.route("/give-rating/:movieId/:rating").post(verifyJwt, addMovieRating);
 ratingRouter.route("/get-avg-rating/:movieId").get(getAverageRating);
+ratingRouter.route("/edit-rating/:movieId/:ratingId/:rating").patch(verifyJwt, editRating);
+ratingRouter.route("/delete-rating/:ratingId").delete(verifyJwt, deleteRating);
 
 export { ratingRouter };
