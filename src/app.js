b20d66744f813cecb478253056b650e250bc068f
Muheeth <abdulmuheeth88@gmail.com>
2025-07-30 02:09:47 +0530

    Added Swagger

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ import { notFoundMiddleware } from "./middlewares/not-found.middleware.js";
 const app = express();
 
 app.use(
-	cors({
-		origin: process.env.CORS_ORIGIN,
-		credentials: true,
-	})
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
 );
 
 app.use(express.json({ limit: "16kb" }));
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-	res.send("<h1>Hii There!</h1>");
+  res.send("<h1>Hii There!</h1>");
 });
 
 // Routes Import
@@ -30,6 +30,24 @@ import { favouritesRouter } from "./routes/favourites.routes.js";
 import { reviewRouter } from "./routes/review.routes.js";
 import { ratingRouter } from "./routes/rating.routes.js";
 
+// Swagger setup
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const swaggerJsonPath = path.join(__dirname, "../swagger.json");
+const swaggerDefinition = JSON.parse(fs.readFileSync(swaggerJsonPath, "utf-8"));
+
+const swaggerSpec = swaggerJsdoc({
+  definition: swaggerDefinition,
+  apis: [], // You can add paths to your route files here for auto-generation
+});
+
 // Routes Declaration
 app.use("/api/users", userRouter);
 app.use("/api/movies", movieRouter);
@@ -38,6 +56,9 @@ app.use("/api/genres", genreRouter);
 app.use("/api/watchlist", watchlistRouter);
 app.use("/api/favourites", favouritesRouter);
 app.use("/api/ratings", ratingRouter);
+
+// Swagger docs route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(notFoundMiddleware);
 
